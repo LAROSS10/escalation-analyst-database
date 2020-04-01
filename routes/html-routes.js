1adf07f47721f31603adb2d4743cf28c7c9625f8
Josiah <josiahkj123@gmail.com>
2020-03-31 20:32:51 -0500

    Updated html routing to known htmls

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -6,24 +6,25 @@ var isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
   app.get("/", function(req, res) {
-    // If the user already has an account send them to the members page
+    // If the user already has signed in, send them to the form page
     if (req.user) {
-      res.redirect("/members");
+      res.redirect("/form");
+    }
+    //otherwise send them to the login page to login
+    res.sendFile(path.join(__dirname, "../public/login.html"));
+  });
+
+  app.get("/signup", function(req, res) {
+    // If the user is already logged in
+    if (req.user) {
+      res.redirect("/form");
     }
     res.sendFile(path.join(__dirname, "../public/signup.html"));
   });
 
-  app.get("/login", function(req, res) {
-    // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/members");
-    }
-    res.sendFile(path.join(__dirname, "../public/login.html"));
-  });
-
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/members", isAuthenticated, function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/members.html"));
+  app.get("/form", isAuthenticated, function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/form.html"));
   });
 };
