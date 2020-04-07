fd96fd5a9d2f1d8a95e34f2242454be77685ea4a
yarasbini <yarasbinisy19@gmail.com>
2020-04-07 18:42:52 -0500

    vhanges on issue js

--- public/js/issues.js
+++ public/js/issues.js
@@ -1,8 +1,8 @@
 //document ready function
 $(document).ready(function() {
   //variable and pointers declarations
-  const $unresolved = $(".first-box");
-  const $resolved = $(".second-box");
+  // const $unresolved = $(".first-box");
+  // const $resolved = $(".second-box");
   const issues = [];
 
   //initialize all our issues
