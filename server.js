e7ad484fa32f37fc452c24fc28d7a8401a691706
Lori Ross <laross70@gmail.com>
2020-04-07 20:39:22 -0500

    added faker npm for creating fake database data

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var express = require("express");
 var session = require("express-session");
 // Requiring passport as we've configured it
 var passport = require("./config/passport");
+// var faker = require("faker");
 
 // Setting up port and requiring models for syncing
 var PORT = process.env.PORT || 8080;
